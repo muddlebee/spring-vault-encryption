7583d58609cd70446ea9acf7011195a8c71ce0fd
Anwesh <anweshknayak@gmail.com>
2022-10-27 12:26:32 +0530

    Update Application.java

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -4,12 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.vault.core.VaultKeyValueOperationsSupport.KeyValueBackend;
 import org.springframework.vault.core.VaultSysOperations;
 import org.springframework.vault.core.VaultTemplate;
 import org.springframework.vault.core.VaultTransitOperations;
 import org.springframework.vault.support.VaultMount;
-import org.springframework.vault.support.VaultResponse;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
@@ -23,15 +21,6 @@ public class Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... strings) throws Exception {
-
-		// You usually would not print a secret to stdout
-		VaultResponse response = vaultTemplate
-				.opsForKeyValue("secret", KeyValueBackend.KV_2).get("github");
-		System.out.println("Value of github.oauth2.key");
-		System.out.println("-------------------------------");
-//		System.out.println(response.getData().get("github.oauth2.key"));
-		System.out.println("-------------------------------");
-		System.out.println();
 
 		// Let's encrypt some data using the Transit backend.
 		VaultTransitOperations transitOperations = vaultTemplate.opsForTransit();
